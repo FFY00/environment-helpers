81ab64816ddd77663c0dc560efd3a256c2ac7699
Filipe Laíns <lains@riseup.net>
2024-04-18 00:40:06 +0100

    install: fix install_wheel due to changes in the introspect module
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- environment_helpers/install.py
+++ environment_helpers/install.py
@@ -15,12 +15,13 @@ def install_wheel(
     wheel: pathlib.Path, interpreter: pathlib.Path, scheme: Optional[str] = None
 ) -> None:
     """Install a wheel file to a Python environment."""
+    introspectable = environment_helpers.introspect.Introspectable(interpreter)
     destination = installer.destinations.SchemeDictionaryDestination(
-        scheme_dict=environment_helpers.introspect.get_scheme(interpreter, scheme),
+        scheme_dict=introspectable.get_scheme(scheme),
         interpreter=os.fspath(interpreter),
         # FIXME: If the launcher kind is None, it means we don't support scripts for this platform.
         #        We set it to posix in that scenario because installer doesn't support this use-case.
-        script_kind=environment_helpers.introspect.get_launcher_kind(interpreter) or 'posix',
+        script_kind=introspectable.get_launcher_kind() or 'posix',
     )
     with installer.sources.WheelFile.open(wheel) as source:
         installer.install(source, destination, additional_metadata={})
