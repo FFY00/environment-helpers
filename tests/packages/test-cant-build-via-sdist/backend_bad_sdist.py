964b9d3280fbb1b917dd4868860c7d377a00731f
Filipe Laíns <lains@riseup.net>
2024-04-29 05:21:43 +0100

    tests: add build module tests
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ tests/packages/test-cant-build-via-sdist/backend_bad_sdist.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: MIT
+
+import os.path
+import tarfile
+import zipfile
+
+
+def build_sdist(sdist_directory, config_settings=None):
+    name = 'test_cant_build_via_sdist-1.0.0'
+    file = f'{name}.tar.gz'
+    with tarfile.open(os.path.join(sdist_directory, file), 'w') as t:
+        t.add('pyproject.toml', f'{name}/pyproject.toml')
+        t.add('backend_bad_sdist.py', f'{name}/backend_bad_sdist.py')
+    return file
+
+
+def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
+    if not os.path.isfile('some-file-that-is-needed-for-build.txt'):
+        raise FileNotFoundError('some-file-that-is-needed-for-build.txt is missing!')
+    # pragma: no cover
+    file = 'test_cant_build_via_sdist-1.0.0-py2.py3-none-any.whl'
+    zipfile.ZipFile(os.path.join(wheel_directory, file), 'w').close()
+    return file
---
+++ tests/test_build.py
@@ -0,0 +1,35 @@
+import build
+import pytest
+
+import environment_helpers.build
+
+
+def test_build_sdist(packages_path, tmp_path):
+    package = packages_path / 'example'
+    sdist = environment_helpers.build.build_sdist(package, tmp_path)
+
+    assert sdist == tmp_path / sdist.name
+    assert sdist.name == 'example-1.2.3.tar.gz'
+
+
+def test_build_wheel(packages_path, tmp_path):
+    package = packages_path / 'example'
+    wheel = environment_helpers.build.build_wheel(package, tmp_path)
+
+    assert wheel == tmp_path / wheel.name
+    assert wheel.name == 'example-1.2.3-py2.py3-none-any.whl'
+
+
+def build_wheel_via_sdist(packages_path, tmp_path):
+    package = packages_path / 'example'
+    wheel = environment_helpers.build.build_wheel_via_sdist(package, tmp_path)
+
+    assert wheel == tmp_path / wheel.name
+    assert wheel.name == 'example-1.2.3-py2.py3-none-any.whl'
+
+
+def build_wheel_via_sdist_fail(packages_path, tmp_path):
+    package = packages_path / 'test-cant-build-via-sdist'
+    with pytest.raises(build.BuildBackendException):
+        environment_helpers.build.build_wheel_via_sdist(package, tmp_path)
+
