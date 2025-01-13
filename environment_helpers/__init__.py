ba2aaccddda32ea6861607b4f891eeb0bfc563ce
Filipe Laíns <lains@riseup.net>
2025-01-13 03:28:54 +0000

    eh: use Environment.run in Environment.install
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- environment_helpers/__init__.py
+++ environment_helpers/__init__.py
@@ -107,7 +107,7 @@ class Environment(Protocol):
         elif method == 'uv':
             cmd = ['uv', 'pip', 'install', '--python', os.fspath(self.interpreter)]
 
-        subprocess.check_output([*cmd, *requirements])
+        self.run(*cmd, *requirements)
 
 
 class CurrentEnvironment(Environment):
